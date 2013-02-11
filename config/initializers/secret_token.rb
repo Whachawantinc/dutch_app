da13462fcd361d04338fb00d99648158f17db42d
Dutch306 <whachawantinc@gmail.com>
2013-02-10 18:56:08 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DutchApp::Application.config.secret_token = '70a991bdcb51a61acfc2a0af0e953da64c8b2d836438b4cc858d0318898a450c3794cf48dc32fe6d2b39df00a91b5faa118976e5e6311ed89210d00a8d8f83cf'
